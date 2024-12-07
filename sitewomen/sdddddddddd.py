9112050f652955687607c66eed6bbde8ffd5cdd8
Docha <Docha@gmail.com>
2024-12-07 20:53:05 +0300

    Update catalog.html

--- sitewomen/sdddddddddd.py
+++
@@ -1,9 +0,0 @@
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3_mxlnd*^7o(_7q-*)4jj_@b(@c6*@(5b31!3ja-m@u5^uzmhy'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
